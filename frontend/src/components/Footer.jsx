0377d94ba1935b21bdd855c36d8430a44efabc27
NikhilSinghal0 <83538349+NikhilSinghal0@users.noreply.github.com>
2024-03-21 22:16:03 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <p>Made by <a href='https://its-atishay.vercel.app' target='_blank' rel="noreferrer" className='link'>Nikhil Singhal</a></p>
+    </div>
+  )
+}
+
+export default Footer
